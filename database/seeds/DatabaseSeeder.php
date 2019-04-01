a551428364f8208400b1f422fed9a96748a1acf2
dmtr <mcompote@gmail.com>
2019-04-01 18:20:34 +0700

    added how-to comment to dbseed main file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,13 @@
 use Illuminate\Database\Seeder;
 
 
+// source: https://stackoverflow.com/questions/33973967/why-do-i-have-to-run-composer-dump-autoload-command-to-make-migrations-work-in
+// In case you get errors while trying to load Seeder classes, use this approach:
+// -php artisan clear-compiled 
+// -composer dump-autoload
+// -php artisan optimize
+
+
 class DatabaseSeeder extends Seeder
 {
     /**
